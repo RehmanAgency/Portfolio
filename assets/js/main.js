4c0b3165d621b93b899b36baa3c4ed6cdc7fc860
RehmanAgency <rehman.agency20@gmail.com>
2024-12-12 22:35:12 +0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,33 @@
+/*=============== SHOW MENU ===============*/
+
+const navMenu=document.getElementById('nav-menu'),
+navToogle=document.getElementById('nav-toggle'),
+navClose=document.getElementById('nav-close')
+
+if(navToogle){
+    navToogle.addEventListener('click',()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+const navLink=document.querySelectorAll('.nav__link')
+
+const linkAction=()=>{
+    const navMenu=document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=>n.addEventListener('click',linkAction))
+
+
+const blurHeader=()=>{
+    const header =document.getElementById('header')
+    this.scrollY>=50?header.classList.add('blur-header')
+                    :header.classList.remove('blur-header')
+}
+window.addEventListener('scroll',blurHeader)
